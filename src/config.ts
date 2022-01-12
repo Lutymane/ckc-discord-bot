4a407f48c2d227895449fee5365651ccaf762bea
Luty <r89534217610@gmail.com>
2022-01-12 03:08:47 +0300

    use token from command line arg

--- src/config.ts
+++ src/config.ts
@@ -2,9 +2,13 @@ export const tezosRPC = "https://mainnet.api.tez.ie/"
 export const ipfsGate = 'https://cloudflare-ipfs.com/ipfs/'//"https://ipfs.io/ipfs/"
 
 
-export const guildId = '591722149060935690'//'889512433763373126'
+export const guildId =
+    //'591722149060935690'
+    '889512433763373126'
 export const appId = '930567550784372837'
 
 
 import fs from 'fs/promises'
-export const token = await fs.readFile('./token').then(d => d.toString());
+export const token =
+    //await fs.readFile('./token').then(d => d.toString());
+    process.argv[2]
