a383f56f0ee16d2f7df5ff77e990c95cfbedeab9
Luty <r89534217610@gmail.com>
2022-01-12 03:14:47 +0300

    upd

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,16 @@
 export const tezosRPC = "https://mainnet.api.tez.ie/"
 export const ipfsGate = 'https://cloudflare-ipfs.com/ipfs/'//"https://ipfs.io/ipfs/"
 
-
-export const guildId =
-    //'591722149060935690'
-    '889512433763373126'
-export const appId = '930567550784372837'
-
-
-import fs from 'fs/promises'
+//import fs from 'fs/promises'
 export const token =
     //await fs.readFile('./token').then(d => d.toString());
     process.argv[2]
+
+
+export const guildId =
+    //'591722149060935690' //test
+    //'889512433763373126' //ckc
+    process.argv[3]
+export const appId = '930567550784372837'
+
+
